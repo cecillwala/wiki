847650080393260216bdc877da6cb8f393714a26
cecillwala <lwalacecil13@gmail.com>
2024-05-09 22:43:33 +0300

    random page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("CS", views.entry, name="entry")
+    path("CSS", views.entry, name="entry"),
+    path("random", views.random_entry, name="random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,23 +1,73 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import markdown2
+from django import forms
 from . import util
+import random
 
+
+class entryForm(forms.Form):
+    title = forms.CharField(label="Search entry")
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
-
-def entry(request):
-    md_text = util.get_entry('CS')
+    md_text = util.get_entry(entry)
     try:
         content = markdown2.markdown(md_text) 
     except TypeError:
-        return render(request, "encyclopedia/error.html")
-    with open('encyclopedia/entry.html', 'w') as file:
+        return HttpResponse("Page Not Found")
+    with open("encyclopedia/entry.html", 'w') as file:
         file.write(content)
-    return render(request, "encyclopedia/entry.html",
-                  {
-                      "title" : 'CS',
-                      "content" : content
-                  })
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "form" : entryForm()
+    })
+
+def entry(request, entry):
+    if request.method == "POST":
+        form = entryForm(request.POST)
+        if form.is_valid():
+            entry =form.cleaned_data["title"]
+        md_text = util.get_entry(entry)
+        try:
+            content = markdown2.markdown(md_text) 
+        except TypeError:
+            return HttpResponse("Page Not Found")
+        with open("encyclopedia/entry.html", 'w') as file:
+            file.write(content)
+        return render(request, "encyclopedia/entry.html",{
+            "title" : entry,
+            "content": content,
+            "form" :entryForm()
+        })
+    entries = util.list_entries()
+    for entry in entries:
+        md_text = util.get_entry(entry)
+        try:
+            content = markdown2.markdown(md_text) 
+        except TypeError:
+            return HttpResponse("Page Not Found")
+        with open("encyclopedia/entry.html", 'w') as file:
+            file.write(content)
+        return render(request, "encyclopedia/entry.html",{
+            "title" : entry,
+            "entry" : entry,
+            "content": content,
+            "form" :entryForm()
+            })
+
+def random_entry(request, entry):
+    entries = util.list_entries()
+    entry = random.choice(entries)
+    md_text = util.get_entry(entry)
+    try:
+        content = markdown2.markdown(md_text) 
+    except TypeError:
+        return HttpResponse("Page Not Found")
+    with open("encyclopedia/entry.html", 'w') as file:
+        file.write(content)
+    return render(request, "encyclopedia/entry.html",{
+        "title" : entry,
+        "content": content,
+        "form" :entryForm()
+    })
+
